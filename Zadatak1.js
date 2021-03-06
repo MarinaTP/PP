e625401718c692328a6eac67aa24455af5a2d9b6
MarinaTP <43954157+MarinaTP@users.noreply.github.com>
2021-03-06 15:07:16 +0100

    Initial commit

---
+++ Zadatak1.js
@@ -0,0 +1,69 @@
+/*var Vreme = 1;
+var vreme = 3;
+var $novac = 25;
+var 4dana = 4;*/
+
+/*var mood = 'tired';
+console.log(mood);*/
+
+/*var a = 3;
+var b = 5;
+var c = 7;
+var d = 3;
+var result = (a*b-c/d);
+console.log(result);*/
+
+/*var bananas = 1.2;
+var kg = 1000;
+console.log(bananas*kg + 'grams');*/
+
+/*var minutes = 30;
+var seconds = 60;
+console.log(minutes*seconds);*/
+
+/*var memoryGB = 4;
+var articles = 98;
+console.log(memoryGB*1024);
+var totalmemory = memoryGB*1024;
+console.log(totalmemory/articles);*/
+
+/*var myname = 'Marina';
+var age = 36;
+var pet = false;
+
+console.log(myname);
+console.log(age);
+console.log(pet);
+
+console.log(typeof myname);
+console.log(typeof age);
+console.log(typeof pet);
+console.log(typeof myname, typeof age, typeof pet);*/
+
+/*var a = 'number';
+var b = true;
+var c = null;
+var d = 'false';
+var e = 56;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);*/
+
+/*var p = true;
+var q = false;
+console.log(p&&q);
+console.log(p||q);*/
+
+/*var age = 20;
+var a = age <= 0;
+var b = age > 120;
+console.log(a&&b);
+console.log(a||b);*/
+
+/*var speed = 180;
+var a = speed <= 120;
+var b = speed >= 60;
+console.log(a&&b);*/
---
+++ variableTypesOperators.js
@@ -0,0 +1,6 @@
+var boja = 'plava';
+
+
+
+console.log(boja);
+console.log(boja);
